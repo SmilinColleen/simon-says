337bf8504ca1b9dde26e7f77238d54c3b20f0d03
Colleen McCloskey <colleen.michelle.mccloskey@gmail.com>
2013-08-15 19:44:48 -0700

    click button and colored cell appears

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+  $('#get_color').on('click',function(e) {
+    e.preventDefault();
+    console.log('click')
+    var colorChange = $.post('/color');  //ajax for post
+
+    colorChange.done(function(response){
+      var ans = $.parseJSON(response)
+      $("ul li:nth-child("+ans.cell+")").css("background-color", ans.color)
+    });
+  });
 
 });
